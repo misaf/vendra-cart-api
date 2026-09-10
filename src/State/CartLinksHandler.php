0d69ae36fe80fc88c1256d729335543d3348e500
misaf <misaf.1990@gmail.com>
2026-09-10 17:07:32 +0000

    style(product): add blank line between use statements in ProductPrice model

--- src/State/CartLinksHandler.php
+++ src/State/CartLinksHandler.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace Misaf\VendraCartApi\State;
 
-use Illuminate\Support\Arr;
 use ApiPlatform\Laravel\Eloquent\State\LinksHandlerInterface;
 use ApiPlatform\Metadata\CollectionOperationInterface;
 use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Auth;
 use Misaf\VendraCart\Models\Cart;
 
